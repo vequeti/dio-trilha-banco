b062e052eacd4877df8f5224b159d3b310618f67
Vinicius Vequeti <viniciusvequeti@gmail.com>
2023-09-12 22:58:17 -0300

    Data input

--- src/application/ContaBanco.java
+++ src/application/ContaBanco.java
@@ -1,9 +1,30 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaBanco {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Scanner sc = new Scanner(System.in);
+		Locale.setDefault(Locale.US);
+		
+		Integer numeroConta;
+		String agencia;
+		String nomeCliente;
+		Double saldo;
+		
+		System.out.print("Por favor, digite o numero da conta: ");
+		numeroConta = sc.nextInt();
+		System.out.println("Por favor, digite o numero da agencia: ");
+		agencia = sc.next();
+		System.out.println("Por favor, digite seu nome: ");
+		nomeCliente = sc.next();
+		System.out.println("Por favor, digite seu saldo: ");
+		saldo = sc.nextDouble();
+		
+		
+		System.out.println("Ola!" + nomeCliente);
+		sc.close();
 	}
-
 }
